f750a92ee866f20722761aaae34ddb0c6c12b909
ratnikoffst <ratnikov_s@list.ru>
2016-02-16 11:02:30 +0300

    Отработка базы SQLite данные

--- src/com/ratnikoff/BaseConfigurator/Base/DataBaseHelper.java
+++ src/com/ratnikoff/BaseConfigurator/Base/DataBaseHelper.java
@@ -8,13 +8,27 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by SM on 15.02.2016.
  */
 public class DataBaseHelper extends SQLiteOpenHelper {
+    public static final String BASE_NAME = "test.db";
+    public static final int VERSION = 1;
 
-    public static final String TABLE_NAME = "BASECONFIGURATOR";
+    public static final String TABLE_OWNER = "BASE_OWNER"; // Таблица базы заказчика
     public static final String NAME_OWNER = "NAME_OWNER"; //Наименование заказчика
-    public static final String NAME_OBJECT = "NAME_OBJECT"; //Наименование заказчика
+    public static final String INN_OWNER = "INN_OWNER"; //ИНН заказчика
+    public static final String ADRESS_OWNER = "ADRESS_OWNER"; //Адрес заказчика
+    public static final String COMMENT_OWNER = "COMMENT_OWNER"; //Комментарий о  заказчике
+
+
+    public static final String TABLE_OBJECT = "BASE_OBJECT"; // Таблица обьектов заказчика
+    public static final String ID_OWNER = "ID_OWNER"; // ID заказчика для связи с первой таблицей
+    public static final String ID_OWNER_OBJECT = "ID_OWNER"; // ID OWNER  OBJECT ключ для связи с третьей таблицой
+    public static final String NAME_OBJECT = "NAME_OWNER"; //ИНН заказчика
+    public static final String ADRESS_OBJECT = "ADRESS_OBJECT"; //Адрес заказчика
+    public static final String COMMENT_OBJECT = "COMMENT_OBJECT"; //Комментарий о  заказчике
+
 
     public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+
     }
 
     @Override
